803202b145836d1ad064833f49d06cf8a107fd67
Paweł hamryszak <phamryszak91@gmail.com>
2023-05-10 13:48:47 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Martha Alvirez",
+    age: 19,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Abigail Bobbin",
+    age: 35,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Chris Fancy",
+    age: 20,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sam Rattle",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lilly Book",
+    age: 26,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
